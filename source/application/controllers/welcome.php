7043ddf20b1c48cbc7fdcd005f3ddc91aade4b42
aymeric.assier <aymeric.assier@6169330b-aba5-5db4-2061-7a11c89a126b>
2012-01-25 12:24:43 +0000

    Mise à jours maquettes. Mise en place du layout utilisateur + login.css Création du controller Café

--- source/application/controllers/welcome.php
+++ source/application/controllers/welcome.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
+class Welcome extends Cafe {
 	
 	
 	/**
@@ -11,25 +11,13 @@ class Welcome extends CI_Controller {
 		parent::__construct();
 		
 		$this->load->model('user_model');
-		
-		$this->layout->ajouter_css('design');
 	}
 	
 	
 	public function index()
 	{
-		$this->layout->view('acceuil/welcome_message');
-	}
-
-	public function listingUser() {
-		
-		$resultat = $this->user_model->getUtilisateur();
-		
-		$data['users'] = $resultat;
-		
-		$this->layout->view('user_list', $data);
-		
-		
+            $this->layout->ajouter_css('utilisateur/login');
+            $this->layout->view('utilisateur/uLogin');
 	}
 }
 
