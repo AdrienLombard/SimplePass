2b9d9aaaa40ae6529463b6d7c1871a4349d24b64
thibaut.krattinger <thibaut.krattinger@6169330b-aba5-5db4-2061-7a11c89a126b>
2012-01-25 15:04:37 +0000

    renomme le model utilisateur en modelUtilisateur

---
+++ source/application/models/modelUtilisateur.php
@@ -0,0 +1,29 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class modelUtilisateur extends CI_Model {
+	
+	private $tableUser = 'utilisateur';
+	
+	public function getUtilisateur() {
+			return $this->db->select('*')
+							->from($this->tableUser)
+							->get()
+							->result();
+	}
+	
+	/*
+	 * @login
+	 * retourne le mot de passe associé au login passé en paramètre
+	 * @return mdp
+	 */
+	
+	public function getMDP($login){
+			return $this->db->select('mdp')
+							->from($this->tableUser)
+							->where('login', $login)
+							->get()
+							->result();
+	}
+	
+	
+}
