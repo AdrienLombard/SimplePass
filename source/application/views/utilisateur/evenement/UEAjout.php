a3a5a6069faa6c873f566ebe522a64a4dbbf25a7
hajerchrigui@gmail.com <hajerchrigui@gmail.com@6169330b-aba5-5db4-2061-7a11c89a126b>
2012-01-26 22:44:49 +0000

    L'ajout et la modification d'un evenement

--- source/application/views/utilisateur/evenement/UEAjout.php
+++ source/application/views/utilisateur/evenement/UEAjout.php
@@ -4,15 +4,20 @@
 
     <div class="tabs">
         <a href="<?php echo site_url('evenements/index'); ?>">Liste</a>
-        <a href="#" class="current">Ajouter</a>
+        <a href="" class="current">Ajouter</a>
     </div>
 
     <div class="box-full">
 		
 		<aside>
-
+           
             <b>Options :</b>
             <ul>
+				
+				<?php if(isset($ajoute)):?>
+				<div calss="ajoute"><?php echo $ajoute; ?></div>
+				<?php endif; ?>
+				
                 <li><a href="<?php echo site_url('evenements/index'); ?>">Retour</a></li>
             </ul>
 
@@ -20,23 +25,23 @@
 
         <div id="main">
 			
-			<form action="#" method="post">
+			<form action="<?php echo site_url('evenements/ajout'); ?>" method="post">
 				
 				<h2>Nouvel évènement</h2>
 				
 				<label>Nom</label>
-				<input type="text"/>
+				<input type="text" name="nom" />
 				
 				<label>Date début</label>
-				<input type="text"/>
+				<input type="text" name="datedebut"/>
 				
 				<label>Date fin</label>
-				<input type="text"/>
+				<input type="text" name="datefin"/>
 				
 				<input type="submit"/>
 				
 			</form>
-			
+			 
         </div>
 
         <div class="clear"></div>
