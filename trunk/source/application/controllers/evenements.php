acaccfdff4970b4ed30a0ffd4c06111bd94af6b6
hajerchrigui@gmail.com <hajerchrigui@gmail.com@6169330b-aba5-5db4-2061-7a11c89a126b>
2012-01-26 22:44:49 +0000

    L'ajout et la modification d'un evenement

--- trunk/source/application/controllers/evenements.php
+++ trunk/source/application/controllers/evenements.php
@@ -5,6 +5,7 @@ class Evenements extends Cafe {
 	
     public function __construct() {
             parent::__construct();
+			$this->load->model('Evenement');
     }
 
 
@@ -12,25 +13,58 @@ class Evenements extends Cafe {
         // TODO
 		$this->layout->ajouter_css('utilisateur/liste');
         $this->layout->view('utilisateur/evenement/UEIndex');
+		
     }
-    
+    /*@la fonction qui permet de consulter l'evenement
+	 * 
+	 */
     public function voir() {
         // TODO
 		$this->layout->ajouter_css('utilisateur/details');
 		$this->layout->view('utilisateur/evenement/UEVoir');
+		
     }
-    
+    /*@ la fonction qui permet l'ajout d'un evenement
+	 * 
+	 */
     public function ajout() {
-        // TODO
-		$this->layout->view('utilisateur/evenement/UEAjout');
+	
+        $this->layout->view('utilisateur/evenement/UEAjout');
+		$nom=$this->input->post('nom');
+		$datedebut=$this->input->post('datedebut');
+		$datefin=$this->input->post('datefin');
+		 if(isset($datedebut) && !empty($datedebut)  
+				                 && isset($datefin) && !empty($datefin)) {
+				
+				$result=$this->Evenement->ajouterEvenement($nom,$datedebut,$datefin);
+				
+		        $data['ajoute']='Evenement ajoute';
+		
+			}
+		
+		
     }
-    
+	/*@la fonction qui permet de modifier un evenement
+	 * 
+	 */
+	
     public function modification() {
         // TODO
 		$this->layout->view('utilisateur/evenement/UEModification');
+		$nom=$this->input->post('nom');
+		$datedebut=$this->input->post('datedebut');
+		$datefin=$this->input->post('datefin');
+		
+		$resultat = $this->Evenement->modifierEvenement($nom,$datedebut,$datefin);
+		$data['modifier']='Evenement modifier';
+		$this->load->view('utilisateur/evenement/UEModification',$data);
     }
-    
+    /*@la fonction qui permet de supprimer un evenement
+	 * 
+	 */
+	
     public function suppression() {
         // TODO
+		$resultat = $this->Evenement->supprimerEvenenement($id);
     }
 }
--- trunk/source/application/views/utilisateur/evenement/UEModification.php
+++ trunk/source/application/views/utilisateur/evenement/UEModification.php
@@ -14,6 +14,9 @@
 
             <b>Options :</b>
             <ul>
+				<?php if(isset($modifier)):?>
+				<div calss="success"><?php echo $modifier; ?></div>
+				<?php endif; ?>
                 <li><a href="<?php echo site_url('evenements/index'); ?>">Retour</a></li>
             </ul>
 
@@ -21,18 +24,18 @@
 
         <div id="main">
 			
-			<form action="#" method="post">
+			<form action="" method="post">
 				
 				<h2>Coupe du monde de ski hiver</h2>
 				
 				<label>Nom</label>
-				<input type="text" value="Coupe du monde de ski hiver"/>
+				<input type="text" value="Coupe du monde de ski hiver" name="nom"/>
 				
 				<label>Date début</label>
-				<input type="text" value="12 Janvier 2012"/>
+				<input type="text" value="12 Janvier 2012" datedebu="datedebut"/>
 				
 				<label>Date fin</label>
-				<input type="text" value="22 Janvier 2012"/>
+				<input type="text" value="22 Janvier 2012" datefin="datefin"/>
 				
 				<input type="submit"/>
 				
