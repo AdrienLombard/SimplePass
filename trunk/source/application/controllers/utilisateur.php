fd442bc9b632bf4c9c6ee0762e87733cf9a177e8
thibaut.krattinger <thibaut.krattinger@6169330b-aba5-5db4-2061-7a11c89a126b>
2012-01-31 12:54:54 +0000

    controller utilisateur.php creation modelUtilisateur creation modification code ULogin.php

---
+++ trunk/source/application/controllers/utilisateur.php
@@ -0,0 +1,50 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+
+class utilisateur extends Cafe {
+	/**
+	 * constucteur de notre classe de base.
+	 */
+	public function __construct()
+	{
+		parent::__construct();
+		
+		$this->load->model('modelUtilisateur');
+	}
+	
+	
+	public function index($message='')
+	{
+		$data['message'] = $message;
+		echo $data['message'];
+		$this->layout->ajouter_css('utilisateur/login');
+		$this->layout->view('utilisateur/ULogin', $data);
+	}
+	 
+	
+	public function connexionUtilisateur(){
+		$login = $this->input->post('login');
+		$mdp = $this->input->post('mdp');
+		$mdpBD = $this->modelUtilisateur->getMDP($login);
+		if(isset($mdpBD)){
+			if($mdpBD[0]->mdp == $mdp) {
+				//TODO
+				//$this->load->view('');
+			}
+			else {
+				$message = 'Le mot de passe est incorrect.';
+				echo 'BB'.$message;
+				$this->index($message);
+			}
+		}
+		else {
+			$message = 'Le login est incorrect.';
+			echo 'BB'.$message;
+			$this->index($message);
+		}
+		
+	}
+}
+
+/* End of file utilisateur.php */
+/* Location: ./application/controllers/utilisateur.php */
