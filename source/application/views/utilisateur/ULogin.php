d6aa8a03d3139037e392e91d5ad1af6f3e18acf3
thibaut.krattinger <thibaut.krattinger@6169330b-aba5-5db4-2061-7a11c89a126b>
2012-01-31 12:54:54 +0000

    controller utilisateur.php creation modelUtilisateur creation modification code ULogin.php

--- source/application/views/utilisateur/ULogin.php
+++ source/application/views/utilisateur/ULogin.php
@@ -1,16 +1,18 @@
 <div id="login">
-    <form method="post" action="#">
-
+    <form method="post" action="<?php echo site_url('utilisateur/connexionUtilisateur'); ?>">
+		
         <h2>Authentification</h2>
 
         <label>Nom d'utilisateur</label>
-        <input type="text" />
+        <input type="text" name="login"/>
 
         <label>Mot de passe</label>
-        <input type="password" />
+        <input type="password" name="mdp"/>
 
-        <input type="submit" />
-
+        <input type="submit" name="valider"/>
+		
+		<span class="erreur" ><?php if(isset($message)) echo $message; ?></span>
+		
         <div class="clear"></div>
 
     </form>
