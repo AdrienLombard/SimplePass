1c55e39c99593dac89555d921ada3817b7d0aaa2
arnould.mickael@gmail.com <arnould.mickael@gmail.com@6169330b-aba5-5db4-2061-7a11c89a126b>
2012-02-03 08:19:54 +0000

    reparation connexion

--- source/application/controllers/utilisateur.php
+++ source/application/controllers/utilisateur.php
@@ -1,29 +1,49 @@
-<?php
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-Class Utilisateur extends Cafe{
-    
-    
-   public function __construct()
+
+class utilisateur extends Cafe {
+	/**
+	 * constucteur de notre classe de base.
+	 */
+	public function __construct()
 	{
 		parent::__construct();
-		/* on charge le model user*/
+		
+		$this->load->model('modelUtilisateur');
+	}
+	
+	
+	public function index($message='')
+	{
+		$data['message'] = $message;
+		echo $data['message'];
+		$this->layout->ajouter_css('utilisateur/login');
+		$this->layout->view('utilisateur/ULogin', $data);
+	}
+	 
+	
+	public function connexion(){
+		$login = $this->input->post('login');
+		$mdp = $this->input->post('mdp');
+		$mdpBD = $this->modelUtilisateur->getMDP($login);
+		if(isset($mdpBD)){
+			if($mdpBD[0]->mdp == $mdp) {
+				$this->layout->view('utilisateur/UWelcome');
+			}
+			else {
+				$message = 'Le mot de passe est incorrect.';
+				echo 'BB'.$message;
+				$this->index($message);
+			}
+		}
+		else {
+			$message = 'Le login est incorrect.';
+			echo 'BB'.$message;
+			$this->index($message);
+		}
 		
 	}
-        
-   
-  
-      
-        
- public function listingUser() {
-		
-		$resultat = $this->utilisateur_model->getUser();
-		
-		$data['users'] = $resultat;
-		
-		//$this->layout->view('user_list', $data);
-		
-		
-	}
-
 }
-?>
+
+/* End of file utilisateur.php */
+/* Location: ./application/controllers/utilisateur.php */
