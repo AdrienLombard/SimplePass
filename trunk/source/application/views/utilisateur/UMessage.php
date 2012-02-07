7bf89bd18413870aa63f913f2f60df133e1a89b2
aymeric.assier <aymeric.assier@6169330b-aba5-5db4-2061-7a11c89a126b>
2012-02-07 14:11:50 +0000

    Correctif accueil login

---
+++ trunk/source/application/views/utilisateur/UMessage.php
@@ -0,0 +1,18 @@
+<h1><?php echo $titre; ?></h1>
+
+<div class="wrap">
+
+    <div class="box-full">
+
+        <div id="main">
+            
+			<?php echo $message; ?><br/>
+			<a href="<?php echo site_url($redirect); ?>" class="button">Continuer</a>
+			
+        </div>
+
+        <div class="clear"></div>
+
+    </div>
+
+</div>
