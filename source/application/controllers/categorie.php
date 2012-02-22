edf1790b230ed0cd2b6d0da7402dcafe8cd05e3b
hajerchrigui@gmail.com <hajerchrigui@gmail.com@6169330b-aba5-5db4-2061-7a11c89a126b>
2012-02-22 08:57:06 +0000

    Crud Catégorie

---
+++ source/application/controllers/categorie.php
@@ -0,0 +1,160 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Categorie extends Cafe {
+	
+	
+	public function __construct() {
+		
+		parent::__construct();
+		$this->load->model('modelCategorie');
+		$this->load->library('form_validation');
+		$this->layout->ajouter_js('utilisateur/scriptDate');
+		
+		
+	}
+
+
+	public function index() {
+		
+		$data['categories'] = $this->modelCategorie->getCategorieMere();
+		$this->layout->view('utilisateur/categorie/UCIndex', $data);
+		
+	}
+
+	
+	public function liste() {
+		
+		$this->layout->ajouter_css('utilisateur/liste');
+		
+		$data['resultats']=$this->modelCategorie->getCategorieMere();
+		
+		$this->layout->view('utilisateur/categorie/UCIndex', $data);
+	}
+	
+	public function voir($id) {
+		
+		$this->layout->ajouter_css('utilisateur/details');
+		$data['nom']=$this->modelCategorie->getCategorieMereid($id);
+		$data['resultats']=$this->modelCategorie->getSousCategorie($id);
+		$data['id'] = $id;
+		$this->layout->view('utilisateur/categorie/VoirCateg', $data);
+	}
+	
+	public function ajouter($values='') {
+		
+		$data['info'] = $values;
+		$data['categories']=$this->modelCategorie->getCategorie();
+		$this->layout->view('utilisateur/categorie/AjoutCateg', $data);
+		
+	}
+	public function exeAjouter() {
+	$config = array(
+		array(
+					'field'   => 'nom',
+					'label'   => 'Nom', 
+					'rules'   => 'required'
+		));
+		$this->form_validation->set_rules($config);
+		
+		$nom 		= $this->input->post('nom');
+		
+		echo $nom;
+		
+		if ($this->form_validation->run() == true ) {
+	    	$data['titre']		= 'Ajout';
+			$data['message']	= 'Votre évènement à bien été ajouté.';
+			$data['redirect'] 	= 'evenement/liste';
+			$idcategorie=$_POST['categories'];
+			
+			if( $idcategorie!=-1)
+			$this->modelCategorie->ajouter( $nom,$idcategorie);
+			else
+				$this->modelCategorie->ajouter( $nom,NULL);
+			$this->layout->view('utilisateur/UMessage', $data);	 
+		}
+		else {
+			
+			$values->nom		= $nom;
+			
+			$this->ajouter($values);
+		}
+		
+	}
+	
+	public function supprimer($id) {
+		
+		$categories =$this->modelCategorie->getCategorieMereid($id);
+	
+		foreach ($categories as $categorie)
+		{
+			
+			echo $categorie->surcategorie;
+                if($categorie->surcategorie==null)
+				{
+					$this->modelCategorie->supprimerCategorie($id);
+                    $this->modelCategorie->supprimersousCategorie($categorie->idcategorie);
+				}
+
+	     else 
+			$this->modelCategorie->supprimerCategorie($id);
+		}
+		$data['titre']		= 'Suppression';
+		$data['message']	= 'Votre évènement a bien été supprimé.';
+		$data['redirect'] 	= 'evenement/liste';
+		$this->layout->view('utilisateur/UMessage', $data);
+		
+	}
+	
+	public function modifier($id,$re=false) {
+		
+		// Traitement
+		$data['id'] = $id;
+		if($re)
+		{
+			$data['nom'] 		= $re['nom'];
+	   	
+		}
+		else {
+			
+			$reponse = $this->modelCategorie->getCategorieMereid($id);
+			$data['nom']= $reponse[0]->libellecategorie;
+			
+		}
+		$this->layout->view('utilisateur/categorie/UCModifier',$data);
+	}
+	
+	
+	public function exeModifier($id) {
+		$config = array(
+			'field'   => 'nom',
+			'label'   => 'Nom', 
+			'rules'   => 'alpha'
+		);
+		
+		$this->form_validation->set_rules($config);
+		
+		$nom 		= $this->input->post('nom');
+		echo $nom;
+		if ($this->form_validation->run()==true)
+	{ 
+			$resultat = $this->modelCategorie->modifier( $id,$nom );
+			$data['titre']		= 'Modification';
+			$data['message']	= 'Votre catégorie à bien été modifié.';
+			$data['redirect'] 	= 'categorie/liste';
+			$this->layout->view('utilisateur/UMessage', $data);
+		
+
+	}
+		else {
+			
+			$donnees['nom'] = $nom;
+			$this->modifier($id, $donnees);
+		}
+		
+	}
+	
+	public function exeSupprimer()
+	{
+		
+	}
+}
---
+++ source/application/views/utilisateur/categorie/VoirCateg.php
@@ -0,0 +1,47 @@
+<h1>Catégories</h1>
+
+<div class="wrap">
+
+    <div class="tabs">
+        <a href="<?php echo site_url('categorie/liste'); ?>">Liste</a>
+        <a href="<?php echo site_url('categorie/ajouter'); ?>">Ajouter</a>
+		<a href="#" class="current">Détails</a>
+    </div>
+
+    <div class="box-full">
+		
+		<aside>
+
+            <b>Options :</b>
+            <ul>
+                
+				<li><a href="<?php echo site_url('categorie/modifier/'.$id); ?>">Modifier</a></li>
+                <li><a href="<?php echo site_url('categorie/supprimer/'.$id);?>" confirm="Êtes-vous sûr de vouloir supprimer cette catégorie ?">Supprimer</a></li>
+				<li><a href="<?php echo site_url('categorie/index/'.$id); ?>">Retour</a></li>
+            </ul>
+
+        </aside>
+
+        <div id="main">
+			<?php foreach ($nom as $nom1):{?>
+			<h2><?php echo $nom1->libellecategorie ?></h2>
+			<?php } endforeach; ?>
+            <?php foreach ($resultats as $categorie):{?>
+			
+			<table class="details">
+				<tr>
+					<th>Sous catégorie </th>
+					<td><?php echo $categorie->libellecategorie?></td>
+					<td><a href="<?php echo site_url('categorie/voir/'.$categorie->idcategorie ); ?>">Voir</a></td>
+				</tr>
+				
+				<?php } endforeach; ?>
+			</table>
+			
+        </div>
+
+        <div class="clear"></div>
+
+    </div>
+
+</div>
