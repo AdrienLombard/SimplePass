860c470550de3cf74d328547218ffd5b11a69026
lordpampa@gmail.com <lordpampa@gmail.com@6169330b-aba5-5db4-2061-7a11c89a126b>
2012-02-27 09:46:32 +0000

    Mise à jour du CRUD zone.

--- source/application/views/utilisateur/zone/UZVoir.php
+++ source/application/views/utilisateur/zone/UZVoir.php
@@ -11,17 +11,21 @@
     <div class="box-full">
 		
 		<aside>
- 
-			<a href="<?php echo site_url('zone/modifier/'.$id); ?>">Modifier</a>
-            <a href="<?php echo site_url('zone/supprimer/'.$id);?>" confirm="Êtes-vous sûr de vouloir supprimer cet zone ?">Supprimer</a>
+			<?php if($resultats): ?>
+				<a href="<?php echo site_url('zone/modifier/'.$id); ?>">Modifier</a>
+				<a href="<?php echo site_url('zone/supprimer/'.$id);?>" confirm="Êtes-vous sûr de vouloir supprimer cet zone ?">Supprimer</a>
+			<?php endif; ?>
 			<a href="<?php echo site_url('zone'); ?>">Retour</a>
-
         </aside>
 
         <div id="main">
-            <?php foreach ($resultats as $resultat): ?>
-				<h2><?php echo $resultat->libellezone; ?></h2>
-			<?php endforeach; ?>
+			<?php if($resultats): ?>
+				<?php foreach ($resultats as $resultat): ?>
+					<h2><?php echo $resultat->libellezone; ?></h2>
+				<?php endforeach; ?>
+			<?php else: ?>
+				Cette zone n'éxiste pas.
+			<?php endif; ?>
 			
 			<?php
 			// <table class="details">
