19588c10db1406fea2a6999c209f47961459208d
lordpampa@gmail.com <lordpampa@gmail.com@6169330b-aba5-5db4-2061-7a11c89a126b>
2012-02-29 09:46:50 +0000

    Blindage du CRUD des évènements.

--- trunk/source/application/views/utilisateur/evenement/UEVoir.php
+++ trunk/source/application/views/utilisateur/evenement/UEVoir.php
@@ -14,100 +14,106 @@
 
             <b>Options :</b>
             <ul>
-				<a href="<?php echo site_url('evenement/modifier/'.$id); ?>">Modifier</a>
-				<a href="<?php echo site_url('evenement/supprimer/'.$id);?>" confirm="Êtes-vous sûr de vouloir supprimer cet évènement ?">Supprimer</a>
+				<?php if($resultats): ?>
+					<a href="<?php echo site_url('evenement/modifier/'.$id); ?>">Modifier</a>
+					<a href="<?php echo site_url('evenement/supprimer/'.$id);?>" confirm="Êtes-vous sûr de vouloir supprimer cet évènement ?">Supprimer</a>
+				<?php endif; ?>
 				<a href="<?php echo site_url('evenement/liste'); ?>">Retour</a>
 			</ul>
 
         </aside>
 
         <div id="main">
-            <?php foreach ($resultats as $resultat):{?>
-			<h2><?php echo $resultat->libelleevenement?></h2>
-			
-			<table class="details">
-				<tr>
-					<th>Date début</th>
-					<td><?php echo display_date($resultat->datedebut)?></td>
-				</tr>
-				<tr>
-					<th>Date fin</th>
-					<td><?php echo display_date($resultat->datefin)?></td>
-				</tr>
-				<?php } endforeach; ?>
-			</table>
-			
-			<table class="listeCategorieEvent">
-				<thead>
-					<th></th>
-					<?php foreach($listeZones as $zone): ?>
-					
-						<th class="rotate">
-							<div class="itemRotate" zone="<?php echo $zone->idzone; ?>"><?php echo $zone->libellezone; ?></div>
-						</th>
-						
-					<?php endforeach; ?>
+			<?php if($resultats): ?>
+				<?php foreach ($resultats as $resultat):{?>
+				<h2><?php echo $resultat->libelleevenement?></h2>
 
-						<th class="rotate">
-							<div class="itemRotate"></div>
-						</th>
-				
-				</thead>
-				
-				<tbody>
-					
-					<tr class="ligneCodeZone">
-						<td class="titreCodeDeLaZone">code de la zone :</td>
-						<?php foreach($listeZones as $zone): ?>
-							<td>
-								<input type="text" 
-									   maxlength="3" 
-									   name="<?php echo 'code_' . $zone->idzone; ?>" 
-									   zone="<?php echo $zone->idzone; ?>" 
-									   class="codeZone"
-									   value="<?php echo $zone->codezone; ?>"
-									   disabled="disabled" />
-							</td>
-						<?php endforeach; ?>
+				<table class="details">
+					<tr>
+						<th>Date début</th>
+						<td><?php echo display_date($resultat->datedebut)?></td>
 					</tr>
-					
-					<?php if(isset($listeCategorie)): ?>
-						<?php foreach ($listeCategorie as $categorie): ?>
-						<tr class="ligneChoixZoneCat">
+					<tr>
+						<th>Date fin</th>
+						<td><?php echo display_date($resultat->datefin)?></td>
+					</tr>
+					<?php } endforeach; ?>
+				</table>
 
-							<td style="padding-left: <?php echo ($categorie['depth'] * 20) + 10; ?>px">
-								<?php echo $categorie['db']->libellecategorie?>
-							</td>
+				<table class="listeCategorieEvent">
+					<thead>
+						<th></th>
+						<?php foreach($listeZones as $zone): ?>
 
-							<?php foreach($listeZones as $zone): ?>
-								
-								<?php if(isset($listeCatgorieZone[$categorie['db']->idcategorie][$zone->idzone])): ?>
-									
-									<td>
-										<input type="checkbox"
-											   name="<?php echo $categorie['db']->idcategorie . '_' . $zone->idzone; ?>"
-											   cat="<?php echo $categorie['db']->idcategorie; ?>"
-											   zone="<?php echo $zone->idzone; ?>"
-											   checked="checked"
-											   disabled="disabled" />
-									</td>
-								
-								<?php else: ?>
-								
-									<td>
-										
-									</td>
-								
-								<?php endif; ?>
+							<th class="rotate">
+								<div class="itemRotate" zone="<?php echo $zone->idzone; ?>"><?php echo $zone->libellezone; ?></div>
+							</th>
 
-							<?php endforeach; ?>
-
-						</tr>
 						<?php endforeach; ?>
-					<?php endif; ?>
-					
-				</tbody>
-			</table>
+
+							<th class="rotate">
+								<div class="itemRotate"></div>
+							</th>
+
+					</thead>
+
+					<tbody>
+
+						<tr class="ligneCodeZone">
+							<td class="titreCodeDeLaZone">code de la zone :</td>
+							<?php foreach($listeZones as $zone): ?>
+								<td>
+									<input type="text" 
+										   maxlength="3" 
+										   name="<?php echo 'code_' . $zone->idzone; ?>" 
+										   zone="<?php echo $zone->idzone; ?>" 
+										   class="codeZone"
+										   value="<?php echo $zone->codezone; ?>"
+										   disabled="disabled" />
+								</td>
+							<?php endforeach; ?>
+						</tr>
+
+						<?php if(isset($listeCategorie)): ?>
+							<?php foreach ($listeCategorie as $categorie): ?>
+							<tr class="ligneChoixZoneCat">
+
+								<td style="padding-left: <?php echo ($categorie['depth'] * 20) + 10; ?>px">
+									<?php echo $categorie['db']->libellecategorie?>
+								</td>
+
+								<?php foreach($listeZones as $zone): ?>
+
+									<?php if(isset($listeCatgorieZone[$categorie['db']->idcategorie][$zone->idzone])): ?>
+
+										<td>
+											<input type="checkbox"
+												   name="<?php echo $categorie['db']->idcategorie . '_' . $zone->idzone; ?>"
+												   cat="<?php echo $categorie['db']->idcategorie; ?>"
+												   zone="<?php echo $zone->idzone; ?>"
+												   checked="checked"
+												   disabled="disabled" />
+										</td>
+
+									<?php else: ?>
+
+										<td>
+
+										</td>
+
+									<?php endif; ?>
+
+								<?php endforeach; ?>
+
+							</tr>
+							<?php endforeach; ?>
+						<?php endif; ?>
+
+					</tbody>
+				</table>
+			<?php else: ?>
+				Cette catégorie n'existe pas.
+			<?php endif; ?>
 			
         </div>
 
