42c84c16b87da3fe123aab37c9cb1677e1ba236f
aymeric.assier <aymeric.assier@6169330b-aba5-5db4-2061-7a11c89a126b>
2012-03-14 10:04:44 +0000

    Amélioration interface catégorie all-in-one

--- source/assets/js/utilisateur/CRUDCategorie.js
+++ source/assets/js/utilisateur/CRUDCategorie.js
@@ -1,4 +1,25 @@
 $('.modifCat').live('click', function(){
 	var id = $(this).attr('data');
-	$('form[data='+id+']').removeClass('disabled').find('input[type=text]').removeAttr('readonly');
-})
+	$('form[data='+id+']').removeClass('disabled').find('input[type=text]').focus().removeAttr('readonly');
+});
+
+/*
+$('.categorieAllInOne form').live("blur", function(){
+	var init = $(this).attr("init");
+	if(init == $(this).val())
+		$(this).attr('readonly', 'readonly').removeClass('error').parent().addClass('disabled');
+	else
+		$(this).addClass('error');
+});
+*/
+
+/*
+ * Aymeric
+ */
+
+
+
+
+/*
+ * Seb
+ */
