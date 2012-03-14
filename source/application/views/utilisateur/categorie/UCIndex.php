f0a97a1e3902dfad71c68e5aaf14845eb2177965
lordpampa@gmail.com <lordpampa@gmail.com@6169330b-aba5-5db4-2061-7a11c89a126b>
2012-03-14 14:27:27 +0000

    Mise en place du color picker.

--- source/application/views/utilisateur/categorie/UCIndex.php
+++ source/application/views/utilisateur/categorie/UCIndex.php
@@ -40,14 +40,21 @@
 								<span class="colorpicker" init="">&nbsp;</span>
 								
 								<div class="hide picker">
-									<span ref="ffffff" style="background: #ffff99">&nbsp;</span>
-									<span ref="ffffff" style="background: #ff99ff">&nbsp;</span>
-									<span ref="ffffff" style="background: #ffffff">&nbsp;</span>
-									<span ref="ffffff" style="background: #99ffff">&nbsp;</span>
-									<span ref="ffffff" style="background: #9999ff">&nbsp;</span>
-									<span ref="ffffff" style="background: #ff9999">&nbsp;</span>
-									<span ref="ffffff" style="background: #999999">&nbsp;</span>
-									<span ref="ffffff" style="background: #f9f9f9">&nbsp;</span>
+									<span ref="FF0000" style="background: #FF0000">&nbsp;</span>
+									<span ref="32CD32" style="background: #32CD32">&nbsp;</span>
+									<span ref="C71585" style="background: #C71585">&nbsp;</span>
+									<span ref="FF4500" style="background: #FF4500">&nbsp;</span>
+									<span ref="FFFF00" style="background: #FFFF00">&nbsp;</span>
+									<span ref="808000" style="background: #808000">&nbsp;</span>
+									<span ref="008B8B" style="background: #008B8B">&nbsp;</span>
+									<span ref="00FFFF" style="background: #00FFFF">&nbsp;</span>
+									<span ref="0000FF" style="background: #0000FF">&nbsp;</span>
+									<span ref="F4A460" style="background: #F4A460">&nbsp;</span>
+									<span ref="A52A2A" style="background: #A52A2A">&nbsp;</span>
+									<span ref="696969" style="background: #696969">&nbsp;</span>
+									<span ref="BDB76B" style="background: #BDB76B">&nbsp;</span>
+									<span ref="CD5C5C" style="background: #CD5C5C">&nbsp;</span>
+									<span ref="9ACD32" style="background: #9ACD32">&nbsp;</span>
 								</div>
 								
 								<input type="submit" value="valider" />
