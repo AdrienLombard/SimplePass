2bed48cd1d907cc9bab0b1f8250dc8941078af24
adrilomb@gmail.com <adrilomb@gmail.com@6169330b-aba5-5db4-2061-7a11c89a126b>
2012-03-14 17:18:52 +0000

    Ajout de l'impression d'une accréditation

---
+++ source/system/core/The.php
@@ -0,0 +1,24 @@
+<?php
+
+class The extends CI_Controller {
+    
+    public function __construct() {
+        
+        parent::__construct();
+		
+		//$this->output->enable_profiler(true);
+		
+		$this->layout->set_theme('layoutThe');
+        
+        $this->layout->ajouter_css('app/app');
+        $this->layout->ajouter_css('app/forms');
+        
+        $this->layout->set_titre('Courchevel');
+        
+		$this->layout->ajouter_js('jquery-1.7.1.min');
+        
+    }
+    
+}
+
+?>
