237fb735af8eb949f38e0b6a39c70a3b5c807fa9
adrilomb@gmail.com <adrilomb@gmail.com@6169330b-aba5-5db4-2061-7a11c89a126b>
2012-03-23 15:35:54 +0000

    Complétion du fichier de langue française

--- trunk/source/application/language/french/fr_lang.php
+++ trunk/source/application/language/french/fr_lang.php
@@ -1,7 +1,10 @@
 <?php
 
+// Messages d'erreur
 $lang['champRequis'] = 'Le champ %s est obligatoire.';
 $lang['emailValide'] = 'Veuillez rentrer un e-Mail valide.';
+
+// Champs communs aux différents formulaires
 $lang['nomGroupe'] = 'Nom du groupe';
 $lang['nom'] = 'Nom';
 $lang['prenom'] = 'Prénom';
@@ -16,6 +19,8 @@ $lang['categorie'] = 'Catégorie';
 $lang['mail'] = 'Mail';
 $lang['societe'] = 'Société, Organisme ou Publication';
 $lang['photo'] = 'Photo';
+
+// Messages de confirmation
 $lang['titreConfirmeDemande'] = 'Confirmation de demande';
 $lang['confirmeDemande'] = 'Votre demande a bien été prise en compte.<br>Merci de votre pré-enregistrement.';
 $lang['titreConfirmeDemandeGroupe'] = 'Confirmation de demande';
