5773120fe66836d58bb5418983f931a9d90d20d2
aymeric.assier <aymeric.assier@6169330b-aba5-5db4-2061-7a11c89a126b>
2012-04-04 06:21:50 +0000

    Ajout du crop coté lambda, deplacement des présentations dans le dossier "doc"

---
+++ trunk/source/application/views/lambda/LCrop.php
@@ -0,0 +1,55 @@
+<script type="text/javascript">
+	
+	$(document).ready(function(){
+		
+		/*
+		 * Crop d'image
+		 */
+		$('.cropper img').Jcrop({
+			onChange:		changeCoords,
+			aspectRatio:	160 / 240,
+			minSize:		[160, 204],
+			setSelect:		[10, 10, 160, 204]
+		});
+		
+		function changeCoords(c) {
+			$('input[name=x]').val(c.x);
+			$('input[name=y]').val(c.y);
+			$('input[name=w]').val(c.w);
+			$('input[name=h]').val(c.h);
+		}
+
+	});
+	
+</script>
+
+<div class="wrap" style="width: 980px;">
+	
+	<h1><?php echo lang('demandeAccred'); ?></h1>
+	<!--
+	<a href="<?php //echo site_url('inscription/changerLangage/fra/' . str_replace('/', ':', uri_string())); ?>" ><img src="<?php //echo img_url('drapeaux/fra.gif'); ?>" alt="fra" /></a>
+	<a href="<?php //echo site_url('inscription/changerLangage/gbr/' . str_replace('/', ':', uri_string())); ?>" ><img src="<?php //echo img_url('drapeaux/gbr.gif'); ?>" alt="gbr" /></a>
+	-->
+	<div class="box-small">
+
+		<form method="post" action="<?php echo site_url('inscription/exeCrop'); ?>" enctype="multipart/form-data">
+
+			<div class="cropper">
+				<img src="<?php echo site_url('image/generate/' . $client->idclient); ?>" />
+			</div>
+
+			<input type="hidden" name="id" value="<?php echo $client->idclient; ?>" />
+			<input type="hidden" name="x" value="10" />
+			<input type="hidden" name="y" value="10" />
+			<input type="hidden" name="w" value="160" />
+			<input type="hidden" name="h" value="204" />
+
+			<input type="submit" value="Valider les nouvelles dimensions" />
+			
+			<div class="clear"></div>
+
+		</form>
+
+	</div>
+	
+</div>
--- trunk/source/application/views/utilisateur/accreditation/UACrop.php
+++ trunk/source/application/views/utilisateur/accreditation/UACrop.php
@@ -13,9 +13,7 @@
 				<form method="post" action="<?php echo site_url('accreditation/exeCrop'); ?>" enctype="multipart/form-data">
 
 					<div class="cropper">
-						
 						<img src="<?php echo site_url('image/generate/' . $client->idclient); ?>" />
-						
 					</div>
 					
 					<input type="hidden" name="id" value="<?php echo $client->idclient; ?>" />
