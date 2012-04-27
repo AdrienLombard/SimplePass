da057856657c75c365a24f65566b7131091a6b17
adrilomb@gmail.com <adrilomb@gmail.com@6169330b-aba5-5db4-2061-7a11c89a126b>
2012-04-27 13:43:03 +0000

    Correction d'un autre bug d'affichage dans la page d'accueil

--- trunk/source/application/views/utilisateur/UWelcome.php
+++ trunk/source/application/views/utilisateur/UWelcome.php
@@ -31,8 +31,8 @@
 							<strong><?php echo $this->session->userdata('libelleEvenementEnCours'); ?></strong>
 							<br><br>					
 							Actuellement : 
-							<li><?php if(isset($nbAccred[0]->count)) echo $nbAccreds[0]->count; ?> accréditation(s)</li>
-							<li><?php if(isset($nbDemandes[0]->count)) echo $nbDemandes[0]->count; ?> demande(s) en attente<br/></li>
+							<li><?php echo $nbAccreds[0]->count; ?> accréditation(s)</li>
+							<li><?php echo $nbDemandes[0]->count; ?> demande(s) en attente<br/></li>
 							
 							<?php if($evenement[0]->datefin - time() <= 60*60*24*20): ?>
 							<span class="alert">Attention !<br/>Il ne vous reste que <?php echo floor(($evenement[0]->datefin - time()) / 60 / 60 / 24); ?> jours pour valider vos demandes !</span>
